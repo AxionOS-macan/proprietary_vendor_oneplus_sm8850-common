38944e1648aba0f7873dabe894958272d1bb5beb
blazerpaul15 <blazerpaul15@gmail.com>
2026-08-23 18:35:58 +0900

    Revert "sm8850-common: Fix URCC and UAH blob packaging"
    This reverts commit 2a2330e3ba8f07aebd3c5f1b5debcb7d052b9496.

--- sm8850-common-vendor.mk
+++ sm8850-common-vendor.mk
@@ -904,7 +904,7 @@ PRODUCT_PACKAGES += \
     libmmparser_lite \
     libmmrtpdecoder \
     libmmrtpencoder \
-    liboplus-uah-client_system_ext \
+    liboplus-uah-client \
     libostatslog \
     libqcc \
     libqcc_file_agent_sys \
@@ -959,7 +959,6 @@ PRODUCT_PACKAGES += \
     libstc_color_feature \
     libuahcore \
     liburcccore \
-    vendor.oplus.hardware.urcc-V1-ndk_odm \
     vendor.pixelworks.hardware.display@1.0 \
     vendor.pixelworks.hardware.display@1.1 \
     vendor.pixelworks.hardware.display@1.2 \
